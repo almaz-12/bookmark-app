a8f2f68074544c5ef4a8f48c1e9e6a84415580f6
Almaz Khabibullin <adreams2016@yandex.ru>
2025-09-19 10:17:38 +0300

    получение закладок

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -2,38 +2,37 @@
 import type { Category } from '@/interfaces/category.interfaces';
 import { useBookmarkStore } from '@/stores/bookmark.store';
 import { useCategoryStore } from '@/stores/category.store';
-import { onMounted, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const category = ref<Category>();
 const route = useRoute();
-const state = useCategoryStore();
-const stateBookmark = useBookmarkStore();
+const categoryStore = useCategoryStore();
+const bookmarkStore = useBookmarkStore();
 
 watch(
   () => ({
     alias: route.params.alias,
-    categories: state.categories,
+    categories: categoryStore.categories,
   }),
-  (data) => {
-    category.value = state.getCategoryByAlias(data.alias);
+  async (data) => {
+    category.value = categoryStore.getCategoryByAlias(data.alias);
+    if (category.value) {
+      await bookmarkStore.fetchBookmarks(category.value.id);
+    }
   },
 );
 
-onMounted(async () => {
-  await stateBookmark.fetchBookmarks(1);
-});
-
 async function createBook() {
-  await stateBookmark.createBookmark(1);
+  await bookmarkStore.createBookmark(1);
 }
 async function deleteBook() {
-  await stateBookmark.deleteBookmark(1);
+  await bookmarkStore.deleteBookmark(1);
 }
 </script>
 
 <template>
-  <h2>{{ category?.name }}</h2>
+  <h2>{{ category?.name }} ({{ bookmarkStore.bookmarks?.length }})</h2>
   <button @click="createBook">Создать закладку</button>
   <button @click="deleteBook">Удалить закладку</button>
 </template>
